ea9d543c7721236966fe85c76ed40d586f33c101
Nazar Pechevistiy <nazarpechka@gmail.com>
2018-04-01 22:08:02 +0300

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -10,7 +10,7 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2
 #define CLIENT_VERSION_MINOR       3
-#define CLIENT_VERSION_REVISION    1
+#define CLIENT_VERSION_REVISION    3
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
@@ -18,7 +18,7 @@
 
 // Copyright year (2014-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 
