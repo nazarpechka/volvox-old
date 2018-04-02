a89e19dec8b1c7afb665a856661726bc5a3092b7
Nazar Pechevistiy <nazarpechka@gmail.com>
2018-04-02 14:18:20 +0300

    Update version

--- src/version.h
+++ src/version.h
@@ -26,10 +26,10 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90005;
+static const int PROTOCOL_VERSION = 90006;
 
 // clients with smaller version number are disconnected
-static const int MIN_PROTO_VERSION = 90002;
+static const int MIN_PROTO_VERSION = 90006;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
