774019b72983f5db6908767051996614b4a17647
nazarpechka <nazarpechka@gmail.com>
2018-05-28 21:37:01 +0300

    Mac building stuff & update to boost 1.6

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,12 +1,11 @@
 # Remove any old qt5 lingering around
 brew uninstall qt5
+
 # Make sure we have the C libs we need
 brew install boost@1.60 pkg-config automake
 
 # Install some deps
-brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db@4 zlib libevent
-
-brew link boost@1.57 --force
+brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4 zlib libevent
 
 # Make sure our stuff is linked in our path
 brew link automake autoconf
